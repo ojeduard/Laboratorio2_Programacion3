78bb840f217bb8ff8cc42bff19cc4ae6e5d721bf
Eduardo <eduardoaojedap@gmail.com>
2022-09-20 00:24:51 -0600

    Cambios

--- src/View/employeeView.java
+++ src/View/employeeView.java
@@ -3,6 +3,12 @@ package View;
 import javax.swing.*;
 
 public class employeeView {
+    private JPanel addEmployeeView;
     private JTextField textField1;
     private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton saveButton;
+    private JButton cancelButton;
 }
