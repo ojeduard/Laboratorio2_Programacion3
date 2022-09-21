48dd8fabf27b69fb2612365f33a06ab460525875
Eduardo Ojeda <eduardoaojedap@gmail.com>
2022-09-20 18:43:32 -0600

    Se modifica el XML y se agrega otras operaciones

---
+++ src/View/DepartmentView.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.*;
+
+public class DepartmentView {
+    private JTextField textID;
+    private JTextField textName;
+    private JTextField textPhone;
+    private JTextField textSalary;
+    private JTextField textCodeDep;
+    private JButton cancelButton;
+    private JButton saveButton;
+}
